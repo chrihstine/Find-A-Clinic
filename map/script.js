a05cabdaecd9f26a2887f8cbb371f385bfe7078d
chrihstine <e0324139@u.nus.edu>
2023-02-27 17:14:07 +0000

    created layer, markers, and icon for dentist (nonfunctional)

--- map/script.js
+++ map/script.js
@@ -1,3 +1,24 @@
+//creating layer
+let dentistLayer = L.markerClusterGroup({
+    iconCreateFunction: function(cluster){
+        return L.divIcon({
+            html:`<div class="dentistMarkerIcon">
+            ${cluster.getChildCount()}
+            </div>`
+        })
+    }
+});
+
+dentistLayer.addTo(map);
+
+//icon
+const dentistIcon = L.icon({
+    iconUrl: 'images/dentistIcon.png',
+    iconSize: [45,45],
+    iconAnchor:[23,45],
+    popupAnchor: [0,0]
+})
+
 async function main() {
 
     function init() {
@@ -12,14 +33,42 @@ async function main() {
                 let center = map.getBounds().getCenter(); // why? maybe reconsider
                 
                 let dentistResponse = await search(center.lat, center.lng, query, 15007);
-                let gpResponse = await search(center.lat, center.lng, query, 15033);
+                let tcmResponse = await search(center.lat, center.lng, query, 15003);
+                let gpResponse = await search(center.lat, center.lng, query, 15011);
                 //console.log(response);
                 console.log(dentistResponse.results);
+                console.log(tcmResponse.results);
                 console.log(gpResponse.results);
+                
                 searchResultLayer.clearLayers();
 
                 //for dentist
                 for (let eachVenue of dentistResponse.results) {
+                    // console.log(eachVenue)
+                    let coordinate = [ eachVenue.geocodes.main.latitude, eachVenue.geocodes.main.longitude ];
+                    let dentistMarker = L.marker(coordinate);
+                    dentistMarker.bindPopup(`<div><h1>${eachVenue.name}</h1></div>`)
+                    searchResultLayer.addLayer(marker)
+                    dentistMarker.addTo(searchResultLayer); //if we create dentistLayer then it becomes
+                    // search result layer? searchresultlayer is the base layer ?
+                    //add dentistmarker to dentistlayer
+
+                    // add the search result to #search-results
+                    let resultElement = document.createElement('div');
+                    let searchResultElement = document.querySelector('#searchResultElement');
+                    resultElement.className="search-result";
+                    resultElement.innerHTML = eachVenue.name;
+
+                    resultElement.addEventListener('click', function(){
+                        map.flyTo(coordinate, 16);
+                        marker.openPopup();
+                    })
+
+                    searchResultElement.appendChild(resultElement);
+                }
+                
+                //for tcm
+                for (let eachVenue of tcmResponse.results) {
                     // console.log(eachVenue)
                     let coordinate = [ eachVenue.geocodes.main.latitude, eachVenue.geocodes.main.longitude ];
                     let marker = L.marker(coordinate);
@@ -40,7 +89,7 @@ async function main() {
 
                     searchResultElement.appendChild(resultElement);
                 }
-                
+
                 //for gp
                 for (let eachVenue of gpResponse.results) {
                     // console.log(eachVenue)
@@ -75,13 +124,8 @@ async function main() {
         })
     };
 
-
-
     init();
 }
-
-
-
 
 
 main();
